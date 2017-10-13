a8c154162c35507223edd8164235258031a10cc7
Minghao Ni <nminghao.gmail.com>
2017-10-13 19:24:29 +0900

    initilize a discovery object

--- discovery.py
+++ discovery.py
@@ -1,3 +1,11 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 import csv
+from watson_developer_cloud import DiscoveryV1
+
+# TODO: Replace with your own credentials
+discovery = DiscoveryV1{
+    username = '75f48542-23da-4e17-a5ed-af780c16cc92',
+    password = 'q6e5a4U4nsqS',
+    version = '2017-09-01'
+}
