340e29927a6c65a28219ae30ad39cff7385d7e0e
Minghao Ni <nminghao@gmail.com>
2017-10-14 22:43:08 +0900

    add train function

--- discovery.py
+++ discovery.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-# max-line-length = 120
+# [pep8] max-line-length = 120
+import os
 import csv
 import json
 import subprocess
@@ -36,7 +37,7 @@ def upload(csv_file, environment_id, collection_id, debug=False):
 
         for row in csv_reader:
 
-            dict_row = {'id': int(row[0]),
+            dict_row = {'id': row[0],
                         'question': row[1],
                         'answer': row[2]}
             json_row = json.dumps(dict_row)
@@ -61,11 +62,69 @@ def upload(csv_file, environment_id, collection_id, debug=False):
 
             time.sleep(0.1)
 
-        print("**Upload Successfully!**")
+    print("**Upload Successfully!**")
+    # remove temporary json file
+    if os.path.exists(row_upload_file):
+        os.remove(row_upload_file)
+        print("Temporary File Removed!".format(row_upload_file))
+
+
+def train(csv_file, environment_id, collection_id, debug=False):
+    """upload csv file to train the discovery
+    **PLEASE CHECK THE ENCODING OF CSV FILE.**
+    It is ALWAYS a good practise to save a csv/txt file in UTF-8 encoding.
+
+    :param csv_file: path of csv file
+    :type csv_file: string of directory
+    :param environment_id: id of environment to upload to
+    :type environment_id: string
+    :param collection_id: id of collection to upload to
+    :type collection_id: string
+    """
+
+    TRAIN_URL = '/v1/environments/{}/collections/{}/training_data?version={}'.\
+                format(environment_id, collection_id, VERSON)
+
+    train_query_file = 'train.json'
+    url = BASE_URL + TRAIN_URL
+    with open(csv_file, newline='') as csvfile:
+        question_relevance = csv.reader(csvfile, delimiter=',')
+
+        # upload training data
+        print("Uploading Training Data ...")
+        for row in question_relevance:
+            train_query = {'natural_language_query': row[0].replace(r"'", r"\'"),
+                           'examples': [{'document_id': row[i],
+                                         'relevance': int(row[i+1])}
+                                        for i in range(1, len(row), 2) if row[i]]}
+
+            with open(train_query_file, 'w') as train_file:
+                json.dump(train_query, train_file)
+
+            curl_cmd = 'curl -X POST -u "{}":"{}" -H "Content-Type: application/json" -d@{} "{}"'\
+                       .format(CRENDIALS['username'], CRENDIALS['password'], train_query_file, url)
+
+            process = subprocess.Popen(shlex.split(curl_cmd), stdout=subprocess.PIPE)
+            output = process.communicate()
+
+            if debug:
+                print(output)
+
+            time.sleep(0.1)
+
+    print("Training Data Upload Complete!")
+    # remove temporary json file
+    if os.path.exists(train_query_file):
+        os.remove(train_query_file)
+        print("Temporary File Removed!".format(train_query_file))
 
 
 if __name__ == '__main__':
 
     upload(csv_file='_docs/index.csv',
            environment_id='af3e2cb0-4fee-419a-933c-256a0d473266',
-           collection_id='6072d7a3-91cb-4bbb-b6eb-597916af93c4')
+           collection_id='33d51e38-40f4-4355-bb0e-a81619d137b3')
+
+    train(csv_file='_docs/training.csv',
+          environment_id='af3e2cb0-4fee-419a-933c-256a0d473266',
+          collection_id='33d51e38-40f4-4355-bb0e-a81619d137b3')
